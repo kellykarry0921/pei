064dfe6118c9f049688a0bcc23b777b8b52824b8
kellykarry0921 <s1102297@gm.pu.edu.tw>
2022-11-04 22:12:31 +0800

    make it better

--- index.py
+++ index.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    homepage = "<h1>趙姵柔Python+flask+Vercel網頁</h1>"
+    homepage = "<h1>趙姵柔Python網頁</h1>"
     homepage += "<a href=/mis>MIS</a><br>"
     homepage += "<a href=/today>顯示日期時間</a><br>"
     homepage += "<a href=/welcome?nick=kelly>傳送使用者暱稱</a><br>"
